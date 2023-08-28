2537cee1ab34e0b092afdefe1bbdbf8288eed75b
Fred Morris <consulting@m3047.net>
2023-08-28 10:38:33 -0700

    Comprehensive tests for FQDNs and Idents.

--- python/test/fqdn_vs_ident.py
+++ python/test/fqdn_vs_ident.py
@@ -22,6 +22,7 @@ if '..' not in sys.path:
 from trualias.lookup import LookupThing
 import trualias.config as config
 import trualias.parser as parser
+import trualias.alias as alias
 
 def parse(text):
     return config.from_text(parser.MultilineStringLoader(text), raise_on_error=False)
@@ -34,24 +35,72 @@ CONFIG = """
     MATCHES %account%.%ident%.%code%
     WITH ANY(), VOWELS(), ANY();
 
+    ACCOUNT foo
+    MATCHES %account%.%code%.%ident%
+    WITH ANY(), VOWELS(), ANY();
+
+    ACCOUNT foo
+    MATCHES %code%.%account%.%ident%
+    WITH ANY(), VOWELS(), ANY();
+
     ACCOUNT bar
     MATCHES %account%.%fqdn%.%code%
+    WITH ANY(), VOWELS(), ANY();
+
+    ACCOUNT bar
+    MATCHES %account%.%code%.%fqdn%
+    WITH ANY(), VOWELS(), ANY();
+
+    ACCOUNT bar
+    MATCHES %code%.%account%.%fqdn%
     WITH ANY(), VOWELS(), ANY();
 
     ACCOUNT ping
     MATCHES parsely.%ident%.%code%
     WITH ANY(), VOWELS(), ANY();
 
+    ACCOUNT ping
+    MATCHES parsely.%code%.%ident%
+    WITH ANY(), VOWELS(), ANY();
+
+    ACCOUNT ping
+    MATCHES %code%.parsely.%ident%
+    WITH ANY(), VOWELS(), ANY();
+
     ACCOUNT pong
     MATCHES eggplant.%fqdn%.%code%
+    WITH ANY(), VOWELS(), ANY();
+    
+    ACCOUNT pong
+    MATCHES eggplant.%code%.%fqdn%
+    WITH ANY(), VOWELS(), ANY();
+    
+    ACCOUNT pong
+    MATCHES %code%.eggplant.%fqdn%
     WITH ANY(), VOWELS(), ANY();
     
     ACCOUNT zip
     MATCHES walnut-%ident%-%code%
     WITH ANY(), VOWELS(), ANY();
 
+    ACCOUNT zip
+    MATCHES walnut-%code%-%ident%
+    WITH ANY(), VOWELS(), ANY();
+
+    ACCOUNT zip
+    MATCHES %code%-walnut-%ident%
+    WITH ANY(), VOWELS(), ANY();
+
     ACCOUNT zap
     MATCHES almond-%fqdn%-%code%
+    WITH ANY(), VOWELS(), ANY();
+
+    ACCOUNT zap
+    MATCHES almond-%code%-%fqdn%
+    WITH ANY(), VOWELS(), ANY();
+
+    ACCOUNT zap
+    MATCHES %code%-almond-%fqdn%
     WITH ANY(), VOWELS(), ANY();
 """
 
@@ -86,6 +135,120 @@ class TestFQDNvsIdent(unittest.TestCase):
         self.assertEqual(self.looker.find('eggplant.green.beans.g4rxxxx'),'')
         self.assertEqual(self.looker.find('walnut-shrimp-s1mxxxxx'),'')
         self.assertEqual(self.looker.find('almond-mocha.latte-t4lxxxxxx'),'')        
+        return
+
+    def test_ident_underscores(self):
+        """Underscores and dashes alone and in various combinations."""
+        self.assertEqual(self.looker.find('foo.ab.a1b'),'foo')
+        self.assertEqual(self.looker.find('foo.a_b.a1b'),'foo')
+        self.assertEqual(self.looker.find('foo.a__b.a1b'),'foo')
+        self.assertEqual(self.looker.find('foo.a_x_b.a1b'),'foo')
+        self.assertEqual(self.looker.find('parsely.a_b.a1b'),'ping')
+        self.assertEqual(self.looker.find('walnut-a_b-a1b'),'zip')
+        return
+
+    def test_ident_dash(self):
+        """A single label with a dash is not mistaken for either an ident or fqdn."""
+        self.assertEqual(self.looker.find('foo.a-b.a1b'),'')
+        return
+    
+    def test_fqdn_dash(self):
+        """FQDNs with dashes in various combinations."""
+        self.assertEqual(self.looker.find('eggplant.ab.cd.ef.a2f'),'pong')
+        self.assertEqual(self.looker.find('eggplant.a-b.cd.ef.a2f'),'pong')
+        self.assertEqual(self.looker.find('eggplant.a--b.cd.ef.a2f'),'pong')
+        self.assertEqual(self.looker.find('eggplant.a-x-b.cd.ef.a2f'),'pong')
+        self.assertEqual(self.looker.find('eggplant.ab.c-d.ef.a2f'),'pong')
+        return
+
+    def test_fqdn_dash_last_label(self):
+        """FQDN with a dash in the last label."""
+        self.assertEqual(self.looker.find('eggplant.ab.cd.e-f.a2f'),'pong')
+        return
+    
+class TestFqdnMatcher(unittest.TestCase):
+    """Testing of the FQDN Matcher specifically."""
+    
+    def setUp(self):
+        self.matcher = alias.MatchExpression.IDENT_MATCHERS['fqdn']
+        return
+    
+    def test_sanity(self):
+        """Assure that we're looking at the right test item."""
+        self.assertEqual(str(self.matcher), 'fqdn')
+        common = self.matcher.char_sets[0].copy()
+        common.union(*self.matcher.char_sets[1:])
+        self.assertEqual(common, alias.MATCH_ALNUM)
+        self.assertEqual(self.matcher.char_sets[1] - alias.MATCH_ALNUM, set('.-'))
+        return
+
+    def test_fqdn_match(self):
+        """It should match an FQDN."""
+        self.assertEqual(self.matcher('abc.def.ghi'), (0,10))
+        self.assertEqual(self.matcher('xyz,abc.def.ghi,123'), (0,2))
+        self.assertEqual(self.matcher('xyz,abc.def.ghi,123', 4), (4,14))
+        return
+    
+    def test_dashes(self):
+        """Dashes should be allowed within the FQDN."""
+        self.assertEqual(self.matcher('a-b.cd.ef'), (0,8))
+        self.assertEqual(self.matcher('ab.c-d.ef'), (0,8))
+        self.assertEqual(self.matcher('ab.cd.e-f'), (0,8))
+        return
+    
+    def test_leading_trailing(self):
+        """Leading and trailing dashes and dots should be disallowed."""
+        self.assertEqual(self.matcher('.a-b.cd.ef'), None)
+        self.assertEqual(self.matcher('.ab.c-d.ef'), None)
+        self.assertEqual(self.matcher('.ab.cd.e-f'), None)
+        self.assertEqual(self.matcher('a-b.cd.ef.'), (0,8))
+        self.assertEqual(self.matcher('ab.c-d.ef.'), (0,8))
+        self.assertEqual(self.matcher('ab.cd.e-f.'), (0,8))
+        self.assertEqual(self.matcher('-a-b.cd.ef'), None)
+        self.assertEqual(self.matcher('-ab.c-d.ef'), None)
+        self.assertEqual(self.matcher('-ab.cd.e-f'), None)
+        self.assertEqual(self.matcher('a-b.cd.ef-'), (0,8))
+        self.assertEqual(self.matcher('ab.c-d.ef-'), (0,8))
+        self.assertEqual(self.matcher('ab.cd.e-f-'), (0,8))
+        return
+        
+    
+class TestCodePositions(unittest.TestCase):
+    """Test placement of verification code."""
+    
+    def setUp(self):
+        self.config = parse(CONFIG)
+        self.looker = LookupThing(self.config)
+        return
+    
+    def test_code_at_start(self):
+        """Code at the end."""
+        self.assertEqual(self.looker.find('g2r.foo.green'),'foo')
+        self.assertEqual(self.looker.find('g4r.bar.green.beans'),'bar')
+        self.assertEqual(self.looker.find('g2r.parsely.green'),'ping')
+        self.assertEqual(self.looker.find('g4r.eggplant.green.beans'),'pong')
+        self.assertEqual(self.looker.find('s1m-walnut-shrimp'),'zip')
+        self.assertEqual(self.looker.find('t4l-almond-mocha.latte'),'zap')   
+        return
+
+    def test_code_in_middle(self):
+        """Code in the middle."""
+        self.assertEqual(self.looker.find('foo.g2r.green'),'foo')
+        self.assertEqual(self.looker.find('bar.g4r.green.beans'),'bar')
+        self.assertEqual(self.looker.find('parsely.g2r.green'),'ping')
+        self.assertEqual(self.looker.find('eggplant.g4r.green.beans'),'pong')
+        self.assertEqual(self.looker.find('walnut-s1m-shrimp'),'zip')
+        self.assertEqual(self.looker.find('almond-t4l-mocha.latte'),'zap')        
+        return
+
+    def test_code_at_end(self):
+        """Code at the end."""
+        self.assertEqual(self.looker.find('foo.green.g2r'),'foo')
+        self.assertEqual(self.looker.find('bar.green.beans.g4r'),'bar')
+        self.assertEqual(self.looker.find('parsely.green.g2r'),'ping')
+        self.assertEqual(self.looker.find('eggplant.green.beans.g4r'),'pong')
+        self.assertEqual(self.looker.find('walnut-shrimp-s1m'),'zip')
+        self.assertEqual(self.looker.find('almond-mocha.latte-t4l'),'zap')        
         return
 
 if __name__ == '__main__':
